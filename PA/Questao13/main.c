ec14a71016ad959868dd100f7842f2633b0967ab
GabrielMQueiroz <gabrielqueiroz167@gmail.com>
2023-05-02 10:52:11 -0300

    Q13

--- PA/Questao13/main.c
+++ PA/Questao13/main.c
@@ -1,2 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+int comp(const void *a,const void *b){
+    return(*(int*)a- *(int*)b ); 
+}
+
+int main(void){
+    int n;
+    float *x;
+    printf("Digite quantos valores \n");
+    scanf("%d",&n);
+    x=malloc(n*sizeof(int));
+    printf("Digite os valores: ");
+    for(int i =0;i<n;i++){
+        scanf("%f",&x[i]);
+    }
+    qsort(x,n,sizeof(float),comp);
+    for(int i=0;i<n;i++){
+        printf("\n x[%d] == %.1f",i,x[i]);
+    }
+    free(x);
+    return 0;
+}
