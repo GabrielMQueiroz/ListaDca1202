4628bdf6f490f7633c1435d918a72c1107cbf998
GabrielMQueiroz <gabrielqueiroz167@gmail.com>
2023-05-27 16:35:44 -0300

    att

--- Esculotr/main.cpp
+++ Esculotr/main.cpp
@@ -6,9 +6,11 @@ int main()
 
     model.setColor(0,0,0,1);
 
-    model.putBox(0,10,0,50,0,10);
+    model.putBox(25,75,25,50,0,5);
+    model.putBox(25,50,25,75,0,5);
 
-    model.cutBox(0,10,0,10,0,10);
+    model.cutBox(30,40,30,40,0,0);
+    
 
     model.setColor(0,255,0,1);
 
@@ -16,13 +18,13 @@ int main()
 
     model.setColor(0,255,0,1);
 
-    model.putSphere(30,30,30,10);
+    model.putSphere(15,70,30,10);
 
     model.setColor(0,0,255,1);
 
-    model.putEllipsoid(70,70,70,5,10,15);
+    model.putEllipsoid(70,70,70,15,10,15);
 
-    model.cutEllipsoid(80,80,80,5,10,15);
+    model.cutEllipsoid(80,50,80,5,10,15);
 
     model.cutSphere(40,40,40,15);
 
