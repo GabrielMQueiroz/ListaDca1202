3b30cfd63b7bc773d8dd0284051e61f96888b3cc
GabrielMQueiroz <gabrielqueiroz167@gmail.com>
2023-05-27 21:05:07 -0300

    syc

--- Esculotr/main.cpp
+++ Esculotr/main.cpp
@@ -2,11 +2,47 @@
 
 int main()
 {
-    Sculptor model(100,100,100);
+    Sculptor model(200,200,200);
 
-    model.setColor(0,0,0,1);
+    model.setColor(209,123,246,0.25);
+    //corpo celualar
+    model.putSphere(50,0,0,10);
+    model.cutSphere(30,30,30,5);
+    //braços dendtriticos
+    for(int i=0;i<10;i++){
+        model.putSphere(40+i,0,0,3);
+    }
+    for(int i=0;i<15;i++){
+        model.putSphere(65-i,0,0,3);
+    };
+    //dendritos
+    for (int i = 0; i < 15; i++){
+        if(i%2==0){
+            model.putVoxel(0,65+i,2+i);
+        }else{
+            model.putVoxel(2+i,65+i,0);
+        };
+    }
+    for (int i = 0; i < 15; i++){
+        if(i%2==0){
+            model.putVoxel(2+i,67+i,2+i);
+        }else{
+            model.putVoxel(1+i+1,67+i,1+i+1);
+        };
+    }
+    
 
-    model.putBox(25,75,25,50,0,5);
+
+    /*for(int i=0;i<15;i++){
+        for(int j=0;j<15;j++){
+            for(int k=0;k<15;k++){
+                model.putSphere(5+i,5+j,5+k,2);
+            };
+        model.putSphere(5+i,5+j,5,2);
+        };
+    model.putSphere(5+i,5,5,2);
+    };
+    /*model.putBox(25,75,25,50,0,5);
     model.putBox(25,50,25,75,0,5);
 
     model.cutBox(30,40,30,40,0,0);
@@ -28,8 +64,7 @@ int main()
 
     model.cutSphere(40,40,40,15);
 
-    model.cutVoxel(99,99,9);
-
+    model.cutVoxel(99,99,9);*/
     model.writeOFF("model.off");
     return 0;
 }
